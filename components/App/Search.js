73f5a54e6375e8a8c221b7f69ded43cc7b84d822
norvylle <nsuy@up.edu.ph>
2018-11-06 11:46:42 +0800

    added speech

--- components/App/Search.js
+++ components/App/Search.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Input, Form, Item } from 'native-base';
+import { Input, Form, Item, Button, Text } from 'native-base';
+import { Speech } from 'expo';
 
-// const autoBind = require('auto-bind');
+const autoBind = require('auto-bind');
 
 export default class SearchPage extends Component {
     constructor(props){
         super(props)
+        this.state={
+            text:"hello"
+        }
+        autoBind(this)
+    }
+
+    handleSpeak(){
+        Speech.speak(this.state.text,{language:'en', pitch: 1, rate: 1})
     }
     render() {
         return(
             <View>
                 <Form>
-                    <Item><Input placeholder="Search"/></Item>
+                    <Item><Input placeholder="Search" value={this.state.text} onChangeText={(text)=>this.setState({text: text})}/></Item>
                 </Form>
+                <Button onPress={this.handleSpeak}>
+                    <Text>Speak!</Text>
+                </Button>
             </View>
         );
     }
